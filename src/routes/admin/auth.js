0aaadb4839ff201ed48b48803ff3b6617ef34e8a
Vipul <vipulkg30@gmail.com>
2021-05-02 13:30:23 +0530

    SignOut Api Added

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { signup, signin } = require('../../controllers/admin/auth');
+const { requireSignin } = require('../../common-middleware');
+const { signup, signin, signout } = require('../../controllers/admin/auth');
 const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validators/auth');
 const router = express.Router();
 
 
 router.post('/admin/signup',validateSignupRequest, isRequestValidated, signup);
 router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin);
-
+router.post('/admin/signout', requireSignin, signout);
 
 module.exports = router;
