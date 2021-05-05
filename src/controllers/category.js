bc5f9c17675055bba1e7c441568fa12b944e89a6
Vipul <vipulkg30@gmail.com>
2021-05-05 10:56:57 +0530

    jwt token and Category Changes

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -16,6 +16,7 @@ function createCategories(categories, parentId = null){
             _id: cate._id,
             name: cate.name,
             slug: cate.slug,
+            parentId: cate.parentId,
             children: createCategories(categories, cate._id)
         });
     }
