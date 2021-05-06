cb7c6580f65918b4f95a31f2fb716f7e6fd916c1
Vipul <vipulkg30@gmail.com>
2021-05-06 15:02:53 +0530

    Add Api for initialData

--- src/index.server.js
+++ src/index.server.js
@@ -10,6 +10,7 @@ const adminRoutes = require("./routes/admin/auth");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
+const initialDataRoutes = require("./routes/admin/initialData");
 
 
 const app = express();
@@ -36,6 +37,7 @@ app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', cartRoutes);
+app.use('/api', initialDataRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at ${process.env.PORT}`);
---
+++ src/routes/admin/initialData.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { initialData } = require('../../controllers/admin/initialData');
+const router = express.Router();
+
+
+router.post('/initialdata', initialData);
+
+module.exports = router;
