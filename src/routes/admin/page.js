91a42c4cd8a8177d9549d503f811d7fc0678483d
Vipul Goyal <vipulkg30@gmail.com>
2021-05-15 14:43:01 +0530

    mern 29 Backend Fix and Update

--- src/routes/admin/page.js
+++ src/routes/admin/page.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { upload, requireSignin, adminMiddleware } = require('../../common-middleware');
-const { createPage } = require('../../controllers/admin/page');
+const { createPage, getPage } = require('../../controllers/admin/page');
 const router = express.Router();
 
-router.post('/page/create', requireSignin , adminMiddleware, upload.fields([
+router.post(`/page/create`, requireSignin, adminMiddleware, upload.fields([
     { name: 'banners' },
     { name: 'products' }
 ]), createPage)
+
+router.get(`/page/:category/:type`, getPage);
 
 module.exports = router;
