6dec886976259ea4ce76ce2de9d943d4f2a9ae24
Vipul Goyal <vipulkg30@gmail.com>
2021-05-16 12:36:54 +0530

    Admin login in front end removed

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,25 +1,23 @@
 const Product = require('../models/product');
 const shortid = require('shortid');
-const product = require('../models/product');
 const slugify = require('slugify');
 const Category = require('../models/category');
 
 exports.createProduct = (req, res) => {
-    
-    // res.status(200).json({ file: req.files, body: req.body });
-    
-    const {
-        name, price, quantity, description, category, createdBy
-    } = req.body;
 
+    //res.status(200).json( { file: req.files, body: req.body } );
+
+    const {
+        name, price, description, category, quantity, createdBy
+    } = req.body;
     let productPictures = [];
 
-    if(req.files.length > 0) {
+    if(req.files.length > 0){
         productPictures = req.files.map(file => {
             return { img: file.filename }
         });
     }
-    
+
     const product = new Product({
         name: name,
         slug: slugify(name),
@@ -31,12 +29,14 @@ exports.createProduct = (req, res) => {
         createdBy: req.user._id
     });
 
-    product.save((error, product) => {
-        if(error) return res.status(400).json({error});
-        if(product) {
+    product.save(((error, product) => {
+        if(error) return res.status(400).json({ error });
+        if(product){
             res.status(201).json({ product });
         }
-    })
+    }));
+
+
 };
 
 exports.getProductsBySlug = (req, res) => {
@@ -50,10 +50,12 @@ exports.getProductsBySlug = (req, res) => {
 
         if(category){
             Product.find({ category: category._id })
-            .exec((erroe, products) => {
+            .exec((error, products) => {
+
                 if(error){
                     return res.status(400).json({error});
                 }
+
                 if(products.length > 0){
                     res.status(200).json({
                         products,
@@ -66,7 +68,27 @@ exports.getProductsBySlug = (req, res) => {
                         }
                     });
                 }
+
+                
             })
         }
-    })
+        
+
+        
+    });
+}
+
+exports.getProductDetailsById = (req, res) => {
+    const { productId } = req.params;
+    if(productId){
+        Product.findOne({ _id: productId })
+        .exec((error, product) => {
+            if(error) return res.status(400).json({ error });
+            if(product){
+                res.status(200).json({ product });
+            }
+        });
+    }else{
+        return res.status(400).json({ error: 'Params required' });
+    }
 }
