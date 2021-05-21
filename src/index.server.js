b3489bb0d3b824832b4c5638ada9b80f33053256
Vipul Goyal <vipulkg30@gmail.com>
2021-05-21 10:06:48 +0530

    Add Order Confirmation

--- src/index.server.js
+++ src/index.server.js
@@ -13,6 +13,7 @@ const cartRoutes = require("./routes/cart");
 const initialDataRoutes = require("./routes/admin/initialData");
 const pageRoutes = require("./routes/admin/page");
 const addressRoutes = require('./routes/address');
+const orderRoutes = require("./routes/order");
 
 
 const app = express();
@@ -42,6 +43,7 @@ app.use('/api', cartRoutes);
 app.use('/api', initialDataRoutes);
 app.use('/api', pageRoutes);
 app.use('/api', addressRoutes);
+app.use("/api", orderRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
