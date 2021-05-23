e2b9cdadb7e3238074b07f2e68cf889d739175dc
Vipul Goyal <vipulkg30@gmail.com>
2021-05-23 12:37:06 +0530

    admin order handling

--- src/controllers/admin/order.admin.js
+++ src/controllers/admin/order.admin.js
@@ -2,7 +2,7 @@ const Order = require("../../models/order");
 
 exports.updateOrder = (req, res) => {
   Order.updateOne(
-    { user: req.body.userId, "orderStatus.type": req.body.type },
+    { _id: req.body.orderId, "orderStatus.type": req.body.type },
     {
       $set: {
         "orderStatus.$": [
