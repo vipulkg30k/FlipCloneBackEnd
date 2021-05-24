78168c9dd0503f7ba0d4f3f1fc06aa0f3f55cf6b
Vipul Goyal <vipulkg30@gmail.com>
2021-05-24 12:40:17 +0530

    Order Details and ramove item from Cart

--- src/controllers/admin/order.admin.js
+++ src/controllers/admin/order.admin.js
@@ -17,3 +17,10 @@ exports.updateOrder = (req, res) => {
     }
   });
 };
+
+exports.getCustomerOrders = async (req, res) => {
+  const orders = await Order.find({})
+    .populate("items.productId", "name")
+    .exec();
+  res.status(200).json({ orders });
+};
